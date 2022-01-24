c3d24989d2305ba026f9a8018c74fdd8f48c3d13
jjpp43 <junnapark@gmail.com>
2022-01-24 11:32:27 +0000

    Basic react design

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import TopBar from "./topbar/TopBar";
+
+function App() {
+  return (
+    <>
+      <TopBar/>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+    </>
+    
+  );
+}
+
+export default App;
