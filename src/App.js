2270b9ce07b20a5f51651bed9b8298ce9c951f0b
jjpp43 <junnapark@gmail.com>
2022-01-25 11:31:40 +0000

    Account update page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Post from "./components/post/Post";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
 
 function App() {
   return (
     <>
       <TopBar/>
-      <Single/>
+      <Settings/>
     </>
     
   );
--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -4,7 +4,30 @@ import Sidebar from "../sidebar/Sidebar"
 export default function SinglePost() {
     return (
         <div className="singlePost">
-            Single Post
+            <div className="singlePostWrapper">
+                <img className="singlePostImg"
+                    src="https://picsum.photos/id/200/800/600"
+                    alt=""
+                />
+                <h1 className="singlePostTitle">
+                    Loerm Ipsum. blah blah blah whatever title.
+                    <div className="singlePostEdit">
+                        <i className="singlePostIcon far fa-edit"></i>
+                        <i className="singlePostIcon far fa-trash-alt"></i>
+                    </div>
+                </h1>
+                <div className="singlePostInfo">
+                    <span className="singlePostAuthor">Author: <b>Junna</b></span>
+                    <span className="singlePostDate">1 hour ago</span>
+                </div>
+                <p className="singlePostDesc">
+                    Loerm Ipsum. blah blah blah whatever text.
+                    Loerm Ipsum. blah blah blah whatever text.
+                    Loerm Ipsum. blah blah blah whatever text.
+                    Loerm Ipsum. blah blah blah whatever text.
+                    Loerm Ipsum. blah blah blah whatever text.
+                </p>
+            </div>
         </div>
     )
 }
