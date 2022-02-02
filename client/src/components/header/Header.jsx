5dda577add023345d44c67bc70f7b0764018999f
jjpp43 <junnapark@gmail.com>
2022-02-02 06:36:17 +0000

    f

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -4,12 +4,12 @@ export default function Header() {
     return (
         <div className="header">
             <div className="headerTitles">
-                <span className="headerTitleSmall">React & Node</span>
-                <span className="headerTitleLarge">Blog</span>
+                <span className="headerTitleSmall">공군</span>
+                <span className="headerTitleLarge">정보보호병</span>
             </div>
             <img
                 className="headerImg"
-                src="https://picsum.photos/id/1/400/200"
+                src="https://cdn.pixabay.com/photo/2017/12/26/21/19/tech-3041437_960_720.jpg"
                 alt=""
             />
         </div>
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,19 +1,61 @@
-import "./register.css"
+import "./register.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function Register() {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post("https://3000-jjpp43-reactblog-7x3xxlmjek7.ws-us30.gitpod.io/register", {
+                username,
+                email,
+                password
+            });
+            res.data && window.location.replace("/login");
+            console.log(res);
+        } catch (error) {
+            setError(true);
+        }
+    };
+
     return (
         <div className="register">
             <span className="registerTitle">Register</span>
-            <form className="registerForm">
+            <form className="registerForm" onSubmit={handleSubmit}>
                 <label>Email</label>
-                <input type="text" className="registerInput" placeholder="Enter your email" />
+                <input 
+                    type="text" 
+                    className="registerInput" 
+                    placeholder="Enter your email" 
+                    onChange={e=>setEmail(e.target.value)}
+                />
                 <label>Username</label>
-                <input type="text" className="registerInput" placeholder="Enter your username" />
+                <input 
+                    type="text" 
+                    className="registerInput" 
+                    placeholder="Enter your username" 
+                    onChange={e=>setUsername(e.target.value)}
+                />
                 <label>Password</label>
-                <input type="password" className="registerInput" placeholder="Enter your password" />
-                <button className="registerButton">Register</button> 
+                <input 
+                    type="password" 
+                    className="registerInput" 
+                    placeholder="Enter your password" 
+                    onChange={e=>setPassword(e.target.value)}
+                />
+                <button className="registerLoginButton">Register</button> 
             </form>
-            <button className="registerLoginButton">Login</button>
+            <button className="registerButton">
+                <Link className="link" to="/login">
+                    Login
+                </Link>    
+            </button>
         </div>
     )
 }
